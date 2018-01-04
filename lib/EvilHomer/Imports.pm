a95c07a737f27e1a814a92666a3252fee0cf76ba
John Barrett <john@jbrt.org>
2018-01-04 20:31:55 +0000

    Wrap some imports

---
+++ lib/EvilHomer/Imports.pm
@@ -0,0 +1,39 @@
+package EvilHomer::Imports;
+
+use strict;
+use warnings;
+use v5.26.1;
+use feature 'signatures';
+no warnings qw/ experimental::smartmatch experimental::signatures /;
+
+use Import::Into;
+use Carp;
+
+sub import ( $module, $type = 'class' ) {
+    my ( $caller, $filename ) = caller;
+
+    my $i = sub ( $use, @params ) {
+        eval "require $use";
+        $use->import::into( $caller, @params );
+    };
+
+    $i->( $_ ) for ( qw/
+        strict warnings utf8 autodie
+        Carp File::Spec::Functions
+    / );
+
+    for ($type) {
+        when ('role')   { $i->('Moo::Role') }
+        when ('script') { $i->('Moo'); $i->('MooX::Options') }
+        default         { $i->('Moo'); }
+    }
+
+    feature->import::into( $caller, qw/ :5.26.1 signatures state /  );
+
+    warnings->unimport::out_of(
+        $caller,
+        qw/ experimental::smartmatch experimental::signatures /
+    );
+}
+
+1;
